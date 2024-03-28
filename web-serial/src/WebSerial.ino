230bce6c0b44e122cbab8b4616547099bf791409
Muhammet Ozturk <muhammetozturk.219@gmail.com>
2024-03-29 00:43:01 +0300

    WebSerial ornegi eklendi.

---
+++ web-serial/src/WebSerial.ino
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESPAsyncTCP.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #include <AsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
+
+AsyncWebServer server(80);
+
+const char* ssid = "SUPERONLINE_Wi-Fi_1297";
+const char* password = "CskuhSXdDHRx"; 
+
+void recvMsg(uint8_t *data, size_t len){
+  //WebSerial.println("Received Data...");
+  String d = "";
+  for(int i=0; i < len; i++){
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+}
+
+void setup() {
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.printf("WiFi Failed!\n");
+        return;
+    }
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    WebSerial.begin(&server);
+    WebSerial.msgCallback(recvMsg);
+    server.begin();
+}
+
+void loop() {
+    delay(2000);
+    
+}
